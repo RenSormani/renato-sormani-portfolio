a458ae7a7faf813b19c24494cc0a137c847562f3
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-09-14 11:11:46 +0100

    changed contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../../assets/img/contact-img-5.svg";
 import TrackVisibility from "react-on-screen";
@@ -6,43 +5,6 @@ import "animate.css";
 import "./Contact.scss";
 
 const Contact = () => {
-  // const formInitialDetails = {
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   phone: '',
-  //   message: ''
-  // }
-  // const [formDetails, setFormDetails] = useState(formInitialDetails);
-  // const [buttonText, setButtonText] = useState('Send');
-  // const [status, setStatus] = useState({});
-
-  // const onFormUpdate = (category, value) => {
-  //     setFormDetails({
-  //       ...formDetails,
-  //       [category]: value
-  //     })
-  // }
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   setButtonText("Sending...");
-  //   let response = await fetch("http://localhost:5000/contact", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json;charset=utf-8",
-  //     },
-  //     body: JSON.stringify(formDetails),
-  //   });
-  //   setButtonText("Send");
-  //   let result = await response.json();
-  //   setFormDetails(formInitialDetails);
-  //   if (result.code == 200) {
-  //     setStatus({ succes: true, message: 'Message sent successfully'});
-  //   } else {
-  //     setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-  //   }
-  // };
 
   return (
     <section className="contact" id="connect">
