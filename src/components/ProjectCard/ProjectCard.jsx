7e37be7871f3930b8264e52aff191efbc5a182b9
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-10-14 17:30:06 +0100

    added github logos to project

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -2,24 +2,55 @@ import React from "react";
 import "./ProjectCard.scss";
 import { Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
+import { Github } from "react-bootstrap-icons";
 
 export const ProjectCard = ({ title, description, imgUrl, url, urlExtra }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt=""/>
+        <img src={imgUrl} alt="" />
         <div className="proj-txtx">
-          <a href={url}>
-            <h4>{title}</h4>{" "}
-          </a>
+          <h4>{title}</h4>{" "}
           <div className="proj-txtx-copy">
-          <span >{description}</span>
+            <span>{description}</span>
           </div>
-          <div className="arrow">
-          <a href={urlExtra}>
-                        {" "}
-                      <ArrowRightCircle size={30} />
-                    </a>
+          <div className="link-buttons">
+            <div className="arrow">
+              <a href={url}>
+                {" "}
+                <Github size={30} />
+              </a>
+            </div>
+            <div className="arrow">
+              <a href={urlExtra}>
+                {" "}
+                <ArrowRightCircle size={30} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Col>
+  );
+};
+
+export const ProjectCard2 = ({ title, description, imgUrl, url }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} alt="" />
+        <div className="proj-txtx">
+          <h4>{title}</h4>{" "}
+          <div className="proj-txtx-copy">
+            <span>{description}</span>
+          </div>
+          <div className="link-buttons">
+            <div className="arrow">
+              <a href={url}>
+                {" "}
+                <Github size={30} />
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "../ProjectCard/ProjectCard";
+import { ProjectCard2 } from "../ProjectCard/ProjectCard";
 import "./Projects.scss";
 import projImg1 from "../../assets/img/brewdog.jpg";
 import projImg2 from "../../assets/img/new-snake.jpg";
@@ -32,35 +33,40 @@ export const Projects = () => {
   const projects = [
     {
       title: "What's Your Brew, Dog",
-      description: "This React app allows you to search the entire back catalogue of Brewdog beers thanks to some nifty API manipulation.",
+      description:
+        "This React app allows you to search the entire back catalogue of Brewdog beers thanks to some nifty API manipulation.",
       imgUrl: projImg1,
       url: url1,
       urlExtra: urlBrew,
     },
     {
       title: "JRD Electrical Ltd",
-      description: "Commissioned by JRD Electrical Ltd to redesign their website. React, JavaScript and Sass were the order of the day!",
+      description:
+        "Commissioned by JRD Electrical Ltd to redesign their website. React, JavaScript and Sass were the order of the day!",
       imgUrl: projImg4,
       url: url2,
       urlExtra: urlElec,
     },
     {
       title: "It'sssssssss Snake!",
-      description: "Remember Snake on the Nokia 3310? Well I redesigned it using CSS, (minimal) HTML and JavaScript. Game on!",
+      description:
+        "Remember Snake on the Nokia 3310? Well I redesigned it using CSS, (minimal) HTML and JavaScript. Game on!",
       imgUrl: projImg2,
       url: url3,
       urlExtra: urlSnake,
     },
     {
       title: "Hostile Aliens v2",
-      description: "An interesting desktop version of Space Invaders created with all my JavaScript and Sass know-how. The invasion has begun!",
+      description:
+        "An interesting desktop version of Space Invaders created with all my JavaScript and Sass know-how. The invasion has begun!",
       imgUrl: projImg3,
       url: url4,
       urlExtra: urlAlien,
     },
     {
       title: "Oh My Word",
-      description: "What's that? A full-on clone of Wordle - the most popular word-based game around? As the kids would say: word!",
+      description:
+        "What's that? A full-on clone of Wordle - the most popular word-based game around? As the kids would say: word!",
       imgUrl: projImg5,
       url: url5,
       urlExtra: urlWordle,
@@ -73,7 +79,8 @@ export const Projects = () => {
     // },
     {
       title: "Use The Morse, Luke!",
-      description: "We love code. Even Morse Code. This little snazzy Morse Coder uses JavaScript to handle all the heavy lifting. ",
+      description:
+        "We love code. Even Morse Code. This little snazzy Morse Coder uses JavaScript to handle all the heavy lifting. ",
       imgUrl: projImg6,
       url: url6,
       urlExtra: urlMorse,
@@ -81,21 +88,20 @@ export const Projects = () => {
   ];
 
   const projects3 = [
-    
     {
       title: "My GitHub Page...",
-      description: "Head over to my GitHub for even more projects, including ticket trackers, games and my original portfolio made from HTML and CSS.",
+      description:
+        "Head over to my GitHub for even more projects, including ticket trackers, games and my original portfolio made from HTML and CSS.",
       imgUrl: projImg8,
       url: url8,
-      urlExtra: url8,
     },
-   
   ];
 
   const projects2 = [
     {
       title: "The Ice Is Right!",
-      description: "I am currently working on a website for a new icecream parlour in the area called Sorella's. Watch this space...",
+      description:
+        "I am currently working on a website for a new icecream parlour in the area called Sorella's. Watch this space...",
       imgUrl: projImg10,
       url: url10,
       urlExtra: url8,
@@ -115,22 +121,29 @@ export const Projects = () => {
                   }
                 >
                   <div className="project-box">
-                  <h2>My Work</h2>
+                    <h2>My Work</h2>
                     <p>
-                      What's a portfolio without a few examples of your work? Well,
-                      not a portfolio for one. So, without further ado, I
-                      present to you some of my recent projects. Click the arrow when you hover over each image to zoom over to the desired
-                      project's page. Alternatively click on the project title and you will get whisked to its GitHub
-                      page, so you can check out the project's code in all its wonderful glory.
+                      What's a portfolio without a few examples of your work?
+                      Well, not a portfolio for one. So, without further ado, I
+                      present to you some of my most recent projects. Click the
+                      arrow when you hover over each image to zoom over to the
+                      desired project's page. Alternatively click on the Github
+                      icon and you will get whisked to its GitHub page, so you
+                      can check out the project's code in all its wonderful
+                      glory.
                     </p>
                     <div className="logo-container">
-                  <button>
-                  <a href="https://github.com/RenSormani" target="_blank" rel="noreferrer">
-                        {" "}
-                      To my GitHub <ArrowRightCircle size={30} />
-                    </a>
-                  </button>
-                  </div>
+                      <button>
+                        <a
+                          href="https://github.com/RenSormani"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          {" "}
+                          To my GitHub <ArrowRightCircle size={30} />
+                        </a>
+                      </button>
+                    </div>
                   </div>
 
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
@@ -172,7 +185,7 @@ export const Projects = () => {
                       <Tab.Pane eventKey="third">
                         <Row className="development-row">
                           {projects3.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
+                            return <ProjectCard2 key={index} {...project} />;
                           })}
                         </Row>
                       </Tab.Pane>
